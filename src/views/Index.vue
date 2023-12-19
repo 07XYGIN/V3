5b685a6ce4c0d89365037a1b2f2bd08964987c28
07XYGIN <1270784682@qq.com>
2023-12-19 13:26:44 +0800

    first commit

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,57 @@
-<script lang="ts" setup>
+<template>
+  <div class="box">
+    <div class="left">
+      <el-row class="tac">
+        <el-col :span="12">
+          <el-menu active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="2"
+            text-color="#fff" id="ul" v-for="item in navdata" router>
+            <el-menu-item :index="ited.component" v-for="ited in item.children">
+              <el-icon><icon-menu /></el-icon>
+              <span>{{ ited.meta.title }}</span>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
+    </div>
 
+  </div>
+</template>    
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import nav from '../assets/nav.json'
+const navdata = reactive(nav.data)
+console.log(navdata);
 </script>
 
-<template id="temp">
-  <div>
-    111
-  </div>
-</template>
+<style lang="less">
+ul {
+  width: 200px;
+}
 
-<style lang="less" >
+.el-col-12 {
+  max-width: 200px !important;
+  height: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-between;
+}
 
+.box {
+  width: 200px;
+  height: 100vh;
+  background-color: aqua;
+
+  .left {
+    background-color: #545c64;
+    width: 200px;
+    height: 100%;
+
+    .tac {
+      height: 100%;
+    }
+  }
+}
+
+#ul {}
 </style>
