a4be14c5180f2e091c8b53eff46c9dacc9525228
07XYGIN <1270784682@qq.com>
2023-12-19 19:03:54 +0800

    axios + pinia持久化

--- src/utils/resuests.ts
+++ src/utils/resuests.ts
@@ -1,15 +1,17 @@
-import axios from "axios";
-let token = localStorage.getItem('token')
-const instance = axios.create({
-  baseURL: '/api',
-  timeout: 6000,
-  headers: {
-    'X-Custom-Header': 'foobar',
-    'Authorization': token
-  }
-});
-instance.interceptors.response.use((response) => {
-  return response.data;
-}
-);
+import axios from "axios";  
+import { useStore } from '../store/index';
+const store = useStore()
+console.log();
+  
+const instance = axios.create({  
+  baseURL: '/api',  
+  timeout: 6000,  
+  headers: {  
+    'X-Custom-Header': 'foobar',  
+    'Authorization':store.token
+  }  
+});  
+instance.interceptors.response.use((response) => {  
+  return response.data;  
+});  
 export default instance
